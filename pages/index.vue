dde4c2b2af0af1412af5a74e8ba4bc239cfbd354
ash <ash@asboy2035.com>
2025-06-29 12:28:52 +0200

    add pwa meta

--- pages/index.vue
+++ pages/index.vue
@@ -1,22 +1,28 @@
 <script setup>
-  import AboutSidebar from '@/components/home/AboutSidebar.vue'
-  import Navbar from '@/components/premade/navbar/Navbar.vue'
-  import AboutHeader from "@/components/home/AboutHeader.vue"
-  import LocationCard from "@/components/home/cards/LocationCard.vue"
-  import AboutCard from "@/components/home/cards/AboutCard.vue"
-  import Grid from "@/components/layout/Grid.vue"
-  import LangsCard from "@/components/home/cards/LangsCard.vue"
-  import BottomFooter from "@/components/premade/BottomFooter.vue"
+import AboutSidebar from '@/components/home/AboutSidebar.vue'
+import Navbar from '@/components/premade/navbar/Navbar.vue'
+import AboutHeader from "@/components/home/AboutHeader.vue"
+import LocationCard from "@/components/home/cards/LocationCard.vue"
+import AboutCard from "@/components/home/cards/AboutCard.vue"
+import Grid from "@/components/layout/Grid.vue"
+import LangsCard from "@/components/home/cards/LangsCard.vue"
+import BottomFooter from "@/components/premade/BottomFooter.vue"
 
-  useHead({
-    title: 'ash (asboy2035) ∙ Portfolio',
-    meta: [
-      { name: 'description', content: 'My (new) site :)' },
-      { property: 'og:title', content: "ash's Site" },
-      { property: 'og:description', content: 'My (new) site :)' },
-      { property: 'og:image', content: '/images/SitePreview.png' }
-    ]
-  })
+useHead({
+  title: 'ash (asboy2035) ∙ Portfolio',
+  meta: [
+    { name: 'mobile-web-app-capable', content: 'yes' },
+    { name: 'apple-mobile-web-app-capable', content: 'yes' },
+    { name: 'apple-mobile-web-app-status-bar-style', content: 'black-translucent' },
+    { name: 'description', content: 'My (new) site :)' },
+    { property: 'og:title', content: "ash's Site" },
+    { property: 'og:description', content: 'My (new) site :)' },
+    { property: 'og:image', content: '/images/SitePreview.png' }
+  ],
+  link: [
+    { rel: 'manifest', href: '/manifest.webmanifest' }
+  ]
+})
 </script>
 
 <template>
