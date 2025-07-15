a1d6f06bd6759676713492be41fd0c5d6ff6274f
ash <ash@asboy2035.com>
2025-07-15 21:45:05 +0200

    update more links card

--- components/links/MoreLinks.vue
+++ components/links/MoreLinks.vue
@@ -17,21 +17,21 @@
     <h-stack>
       <a href="https://app.warp.dev/referral/KVWRZZ">
         <button>
-          <Icon icon="mingcute:terminal-fill" width="24" height="24" />
+          <Icon icon="mingcute:terminal-fill" />
           Warp
         </button>
       </a>
 
       <a href="https://stats.foldingathome.org/donor/id/732018511">
         <button>
-          <Icon icon="material-symbols:network-node" width="24" height="24" />
+          <Icon icon="material-symbols:network-node" />
           FaH
         </button>
       </a>
 
       <a href="https://www.last.fm/user/asboy_lfm">
         <button>
-          <Icon icon="solar:music-note-outline" width="24" height="24" />
+          <Icon icon="solar:music-note-outline" />
           Last.fm
         </button>
       </a>
@@ -40,7 +40,10 @@
     <h3>Playlists</h3>
     <h-stack>
       <router-link to="/playlists/topsongs">
-        <button>Top Songs</button>
+        <button>
+          <Icon icon="solar:music-note-outline" />
+          Top Songs
+        </button>
       </router-link>
     </h-stack>
   </card>
